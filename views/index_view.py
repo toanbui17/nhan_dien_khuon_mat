c65402fc19b3824347a6d6ecb4cd8de02c4511dc
toanbui17 <toanbui06200@gmail.com>
2024-10-23 17:50:43 +0700

    update_view

--- views/index_view.py
+++ views/index_view.py
@@ -1,9 +1,9 @@
 from flask import render_template, Blueprint
 
 # Khởi tạo một blueprint để nhóm các view của trang home
-home_bp = Blueprint('home', __name__)
+index_bp = Blueprint('index', __name__)
 
 # Định nghĩa view cho trang chủ
-@home_bp.route('/')
-def home():
-    return render_template('home.html')
+@index_bp.route('/webcam')
+def webcam():
+    return render_template('index.html')
