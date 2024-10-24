921b7f83f1d999369a523c8efc6b7847d5cdf8fa
PhamThangbn <thangbn2k1@gmail.com>
2024-10-24 23:20:38 +0700

    “23h11_24_11-2024”

--- webcam.py
+++ webcam.py
@@ -1,21 +1,19 @@
 import cv2
-from flask import Response
-from face_detection import detect_faces, draw_faces  # Import các hàm từ face_utils.py
 
-def generate_frames(camera):
+# Khởi tạo webcam
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
     while True:
-        success, frame = camera.read()
+        success, frame = camera.read()  # Đọc frame từ webcam
         if not success:
             break
         else:
-            faces = detect_faces(frame)  # Nhận diện khuôn mặt
-            frame = draw_faces(frame, faces)  # Vẽ khung nhận diện
-
-            # Encode frame dưới dạng JPEG và trả về luồng ảnh
+            # Chuyển đổi màu BGR sang RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Encode frame thành JPEG
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-def video_feed(camera):
-    return Response(generate_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Trả về frame
